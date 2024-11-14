1eb8ed2617eb40464facd5aa2a8863a3011bcd43
shivshankar7004 <shivshankarmoney@gmail.com>
2024-11-14 23:07:28 +0530

    typewriter

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,31 @@
 import React from "react";
 import myImage from "../photo/1st.png";
+import { Typewriter } from "react-simple-typewriter";
 
 // import Navbar from './Navbar';
 const Home: React.FC = () => {
-  const home_headline1 = "Shivshankar kumar";
-  const home_headline2 = "Software Developer";
-
+ 
   return (
     // <section id="home" className="flex flex-col items-center text-center p-8 bg-gray-200" >
     <section id="home" className="flex h-full  bg-gray-200 ">
       <div className="w-1/2 ml-10 mt-10">
-        <h1 className="text-4xl font-bold m-5 ">Hey, I'm {home_headline1}</h1>
+        <h1 className="text-4xl font-bold m-5 ">
+          Hey, I'm{" "}
+          <span className="text-red-600">
+            <Typewriter
+              loop
+              cursor
+              cursorStyle="_"
+              deleteSpeed={50}
+              delaySpeed={3000}
+              words={[
+                "Shivshankar Kumar",
+                "Software Developer",
+                "Web Developer",
+              ]}
+            />
+          </span>
+        </h1>
         <p className=" ml-5 text-gray-600 ">
           MCA Postgraduate from NITW | Expert in HTML, CSS, JavaScript, React,
           Node.js, TypeScript & Tailwind CSS
